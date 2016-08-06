b62cbd900dcccb513705f3a37d053f17bf456c18
seanreed1111 <seanreed1111@gmail.com>
2016-08-06 17:57:56 -0400

    adds some active record associations for user, artist, and album

--- app/models/album.rb
+++ app/models/album.rb
@@ -1,3 +1,9 @@
 class Album < ActiveRecord::Base
   belongs_to :artist
+
+  has_many :songs
+  accepts_nested_attributes_for :songs
+
+  validates :name, presence: true
+
 end
--- app/models/artist.rb
+++ app/models/artist.rb
@@ -1,2 +1,9 @@
 class Artist < ActiveRecord::Base
+  validates :name, presence: true
+  belongs_to :user
+
+  has_many :albums
+  accepts_nested_attributes_for :albums
+
+
 end
