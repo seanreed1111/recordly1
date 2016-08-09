88097f91b61e571023c3c6888f4c30472203fd7e
seanreed1111 <seanreed1111@gmail.com>
2016-08-09 16:54:57 -0400

    adds sign out link and rspec test fro sign out

--- app/controllers/albums_controller.rb
+++ app/controllers/albums_controller.rb
@@ -1,5 +1,5 @@
 class AlbumsController < ApplicationController
-  before_action :authenticate_user!
+  before_action :authenticate_user!, except: [:index]
   before_action :set_album, only: [:show, :edit, :update, :destroy]
 
   # GET /albums
