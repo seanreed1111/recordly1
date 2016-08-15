3f47ebc2b30fc5e970116261def7102200391ce2
seanreed1111 <seanreed1111@gmail.com>
2016-08-15 10:34:45 -0400

    updates routes!

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,22 @@
 Rails.application.routes.draw do
   devise_for :users
 
-  resources :albums, :artists, :songs do
+  resources :albums do
     resources :favorites, only: [:create, :destroy]
   end
+
+  resources :artists do
+    resources :favorites, only: [:create, :destroy]
+  end
+
+  resources :songs, except: [:create] do
+    resources :favorites, only: [:create, :destroy]
+  end
+
+  resources :albums do
+    resource :songs, only: [:create]
+  end
+
 
   resources :collections
   resources :favorites, only: [:index]
